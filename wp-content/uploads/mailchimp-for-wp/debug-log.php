87f01a68d4f128183f3a02b29fb7cbf90cbc704a
JP <113103403+jaybonesmg@users.noreply.github.com>
2024-08-08 14:51:21 +0200

    New WP installation

--- wp-content/uploads/mailchimp-for-wp/debug-log.php
+++
@@ -1,2 +0,0 @@
-<?php exit; ?>
-[2023-05-12 12:34:49] WARNING: Form 1356 > jp@di*********.co.za is already subscribed to the selected list(s)
--- wp-content/uploads/sucuri/sucuri-auditqueue.php
+++
@@ -1,6 +0,0 @@
-<?php
-// datastore=auditqueue;
-// created_on=1455640621;
-// updated_on=1455640621;
-exit(0);
-?>
